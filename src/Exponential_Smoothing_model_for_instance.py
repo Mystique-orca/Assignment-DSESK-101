94398f51f162dd26fbe32b38f876debd9b93e671
Mystique-orca <38147899+Mystique-orca@users.noreply.github.com>
2021-02-14 23:26:48 +0530

    edit - info

--- src/Exponential_Smoothing_model_for_instance.py
+++ src/Exponential_Smoothing_model_for_instance.py
@@ -1,7 +1,10 @@
 '''
 Author: Sumesh Meppadath
+Feb 13, 2021
+Title: Implementation of regression techniques on time-series data to
+generate future predictions
 Module: Exponential Smoothing model for instance resource prediction
-Date: Feb 13, 2021
+
 
 Reference for freq parameter for time series model
 Alias    Description
