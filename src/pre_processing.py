971d31aa732df7a197fe13fbe17e5900670b540e
Mystique-orca <38147899+Mystique-orca@users.noreply.github.com>
2021-02-14 23:22:23 +0530

    Pre_processing - updated

---
+++ src/pre_processing.py
@@ -0,0 +1,35 @@
+'''
+Author: Sumesh Meppadath
+Module: Pre-processing module
+Date: Feb 13, 2021
+'''
+
+#imports
+import pandas as pd
+
+'''
+Read the csv dumped by input_processing module
+The input is read in fixed sized chunks based on the target label for prediction
+'''
+def read_file(path, label, value):
+    chunk_list=[]
+    for chunk in pd.read_csv(path, chunksize=10000):
+        chunk_list.append(chunk[chunk[label] == value])
+        
+    df = pd.concat(chunk_list)
+    df.drop(df.columns[[0]], axis=1)
+    return df
+
+'''
+Remove the faulty records
+'''
+def clean_dataframe(df):
+    df['timestamp'].str.len() != 28) | (df['group'].str.len() < 6) | (df['group'].str.len() > 7)].index)
+    return df
+
+'''
+Format the timestamp in datetime dtype to enable time series generation
+'''
+def format_datetime(df):
+    df['date'] = pd.to_datetime(df['timestamp']).dt.strftime('%Y%m%d%H%M%S')
+    return df
